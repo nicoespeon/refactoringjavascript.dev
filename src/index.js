5ca2e7dadf6ed4ad90187e6982d99eb2889915ef
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2022-01-05 08:21:55 -0500

    Disable landing page (project paused)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+// import App from './App';
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <p>
+      This project is paused.{" "}
+      <a href="https://twitter.com/nicoespeon">Ping me</a> if you have
+      questions.
+    </p>
+    {/* <App /> */}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
